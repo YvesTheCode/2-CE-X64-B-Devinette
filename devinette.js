09cfa1e558a1ad8b3829ac24638fb610aeb66549
YvesTheCode <alexandre.cleymans@gmail.com>
2025-01-17 13:10:17 +0100

    mode infinir aléatoire + error si rien

--- devinette.js
+++ devinette.js
@@ -51,30 +51,61 @@ butt.value=""
 
 butt.addEventListener("click",function(){
 
-    const tonchoix = repond.value
+    let tonchoix = repond.value
 
     ++tentative
 
-    // alert(nombreMystere)
+    alert(nombreMystere)
     if (isNaN(tonchoix)) {
         ligne[2].innerHTML="Devinette de >>>NOMBRE<<<"
         ligne[4].innerHTML="Trop nul"
     } 
-    
+
+    else if (repond.value.trim() === ""){
+        ligne[2].innerHTML="Faut répondre par contre."
+        ligne[4].innerHTML=">:("
+        --tentative
+    }
+
     else if(tonchoix < nombreMystere){
+
+        ligne[2].innerHTML="Ce n'est pas "+tonchoix+", re-tente."
         ligne[4].innerHTML= "Trop petit"
+
     }
 
     else if(tonchoix > nombreMystere){
+        ligne[2].innerHTML="Ce n'est pas "+tonchoix+", re-tente."
         ligne[4].innerHTML="Trop grand"
     }
 
     else {
+        min = Math.floor(Math.random() * 99) + 1;
+        max = Math.floor(Math.random() * 101) + 100;
         ligne[2].innerHTML="Tu as trouvé en seulement "+ tentative +" tentatives. Rejoue dés maintenant !"
-        ligne[4].innerHTML="Trop fort"
-        nombreMystere = Math.floor(Math.random() * max) + min;
+        ligne[4].innerHTML="Le nouveau nombre se trouve entre "+min+" & "+max
+        nombreMystere = Math.floor(Math.random() * (max-min + 1)) + min;
         tentative=0
-        // alert(nombreMystere)
+        alert(nombreMystere)
     }
         ligne[16].innerHTML = "Nombre d'essais : "+tentative
 });
+
+// let hp = 20
+// let maxhp = 20
+
+// let boucle = setInterval(() => {
+ 
+//   if (hp<=0) {
+//     setTimeout(() => {
+//       maxhp = maxhp+20
+//       hpbar.style.width = 100+"%"
+//       hp = maxhp
+//     }, 0);
+//   }
+
+//   hp = hp-5
+//   hpbar.style.width = (hp/maxhp)*100+"%"
+
+
+// }, 1000);
