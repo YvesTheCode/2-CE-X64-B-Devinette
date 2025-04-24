51ee48b7f7c051664a54c43de847946c0613156b
YvesTheCode <alexandre.cleymans@gmail.com>
2025-04-24 18:22:38 +0200

    Version clean du code (et sans triche)

--- devinette.js
+++ devinette.js
@@ -1,35 +1,3 @@
-// alert("Bienvenue au jeu de devinette de nombre !");
-// let min = 1;
-// let max = 100;
-// const nombreMystere = Math.floor(Math.random() * max) + min; // Nombre entre 1 et 100
-// let tentative = 0;
-// let deviner = false;
-
-// while (!deviner) {
-//   const reponse = prompt("Devinez un nombre entre " + min + " et " + max);
-//   tentative++;
-
-//   if (reponse === null) {
-//     alert("Jeu annulé. Merci d'avoir joué !");
-//     break;
-//   }
-
-//   const nombre = parseInt(reponse, 10);
-
-//   if (isNaN(nombre)) {
-//     alert("Ce n'est pas un nombre valide. Veuillez réessayer !");
-//   } else if (nombre < nombreMystere) {
-//     alert("C'est plus !");
-//   } else if (nombre > nombreMystere) {
-//     alert("C'est moins !");
-//   } else {
-//     alert(
-//       `Félicitations ! Vous avez trouvé le nombre ${nombreMystere} en ${tentative} tentative(s).`
-//     );
-//     deviner = true;
-//   }
-// }
-
 let min = 1;
 let max = 100;
 let nombreMystere = Math.floor(Math.random() * max) + min;
@@ -90,22 +58,3 @@ butt.addEventListener("click",function(){
     }
         ligne[16].innerHTML = "Nombre d'essais : "+tentative
 });
-
-// let hp = 20
-// let maxhp = 20
-
-// let boucle = setInterval(() => {
- 
-//   if (hp<=0) {
-//     setTimeout(() => {
-//       maxhp = maxhp+20
-//       hpbar.style.width = 100+"%"
-//       hp = maxhp
-//     }, 0);
-//   }
-
-//   hp = hp-5
-//   hpbar.style.width = (hp/maxhp)*100+"%"
-
-
-// }, 1000);
